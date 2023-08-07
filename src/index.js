24c3a7671d8355b0b00b28e064baefbd435a0ba8
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-08-07 09:32:04 -0400

    Cleaned code to display a better way to lunch a server

--- src/index.js
+++ src/index.js
@@ -2,45 +2,20 @@
 const express = require('express');
 //instantiate the express function
 const app = express();
-//create a PORT that will be use for the listening function
-const PORT = 3001;
+//use the epress Router object
+let router = express.Router();
+//initialize a port that will be use by the server
+const PORT = 3000;
 
-
-//enabling middleware: is a function that will have all the access for requesting an object, responding to an object, and moving to the next middleware function in the application request-response cycle
-//middlewear can be use to log
-app.use(express.json);
-app.use(express.urlencoded)
-
-app.listen(PORT, ()=> console.log(`The Server is running on PORT: ${PORT}`))
-
-//user list to tesp api
-const userList = [
-    {
-        id:"1",
-        name: "User One",
-        job: "Software Engineer"
-    },
-    {
-        id:"2",
-        name: "User Two",
-        job: "Doctor"
-    },
-    {
-        id:"3",
-        name: "User Thre",
-        job: "Nurse"
-    },
-]
-
-
-//GET Request (Fetch data from the server/db)
-app.get('/api/users',(request, response) =>{
-    response.send(userList);
+//create GET to return a list of cities
+router.get('/', (request, response, next)=>{
+    response.send('Boston')
 })
 
+//configure the router so all routers all prefixed with /api/v1
+app.use('/api', router)
 
-//POST REQUEST (Creating a New resource)
-app.post('/api/users', (request, response)=>{
- console.log(request.body)
- response.send(201)
+//create the server that will listen to the port
+app.listen(PORT, ()=>{
+    console.log(`The Server is running on PORT; ${PORT}`)
 })
